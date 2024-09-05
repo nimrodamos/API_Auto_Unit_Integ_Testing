d0acaca55f473c7233fe7c4d5351dbf8de892d27
nimrod amos <nimrodamos100@gmail.com>
2024-09-05 13:49:59 +0300

    up

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const axios = require("axios");
 require("dotenv").config();
 
 const app = express();
-const port = 3008;
+const port = 3010;
 
 // Route to get all products
 app.get("/products", async (req, res) => {
@@ -137,6 +137,39 @@ app.get("/carts/:id/update", async (req, res) => {
     });
   } catch (err) {
     console.error("API error", err);
+    res.status(500).send("Server Error");
+  }
+});
+
+//use this us a template
+//http://localhost:3010/ANP?title=Test%20Title&price=100&description=Great%20Product&image=https://i.pravatar.cc&category=electronics
+app.get("/ANP", async (req, res) => {
+  try {
+    const title = req.query.title || "default title"; // 'default title' is used if title is not provided
+    const price = req.query.price || "0"; // default price of 0
+    const description = req.query.description || "default description";
+    const image = req.query.image || "https://i.pravatar.cc"; // Default image URL
+    const category = req.query.category || "default category";
+
+    let headers = {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    };
+    const data = {
+      title: title,
+      price: price,
+      description: description,
+      image: image,
+      category: category,
+    };
+    const response = await axios.post(
+      `https://fakestoreapi.com/products`,
+      data,
+      headers
+    );
+    res.json(response.data);
+  } catch (err) {
+    console.error("server error", err);
     res.status(500).send("Server Error");
   }
 });
--- tests/app.test.js
+++ tests/app.test.js
@@ -1,15 +1,18 @@
-//use "node test" to run
+//use "npm test" to run
 const request = require("supertest");
 //const express = require('express');//not in use
 const app = require("../server.js"); // Adjust this path to point to your Express app
 //integration test
-describe("GET /data/auth", () => {
-  it("should respond with status code 200", async () => {
-    const response = await request(app).get("/data/auth");
+describe("GET /VPD/:add and /ANP", () => {
+  it("/VPD/:add should respond with status code 200", async () => {
+    const response = await request(app).get("/carts");
+    expect(response.statusCode).toBe(200);
+  });
+  it("GET /ANP should respond with status code 200", async () => {
+    const response = await request(app).get("/ANP");
     expect(response.statusCode).toBe(200);
   });
 });
-
 //unit test
 describe("unitTest", () => {
   test("checks if 1 + 2 is equal to 3", () => {
